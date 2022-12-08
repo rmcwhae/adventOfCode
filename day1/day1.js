802655718da47afaef7620a33bcec5af1f562e65
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-07 21:24:02 -0800

    Part 1 solution

--- day1/day1.js
+++ day1/day1.js
@@ -2,13 +2,18 @@ const { readFile } = require('node:fs/promises');
 const { resolve } = require('node:path');
 
 async function logFile() {
-  try {
-    const filePath = resolve('./input.txt')
-    const contents = await readFile(filePath, { encoding: 'utf8' })
-    console.log(contents)
-  } catch (err) {
-    console.error(err.message)
-  }
+    try {
+        const filePath = resolve('./input.txt')
+        const contents = await readFile(filePath, { encoding: 'utf8' })
+        const splitByLineGaps = contents
+            .split('\n\n')
+            .map(line => line.split('\n'))
+            .map(values => values.map(value => Number(value)))
+            .map(values => values.reduce((a, b) => a + b, 0))
+        console.log('The most calories carried by an elf is', Math.max(...splitByLineGaps))
+    } catch (err) {
+        console.error(err.message)
+    }
 }
 
 logFile()
