427211ad0f7d8b7c15cc66a7a101b3f073cb9490
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-07 21:05:09 -0800

    File logging

---
+++ day1/day1.js
@@ -0,0 +1,14 @@
+const { readFile } = require('node:fs/promises');
+const { resolve } = require('node:path');
+
+async function logFile() {
+  try {
+    const filePath = resolve('./input.txt')
+    const contents = await readFile(filePath, { encoding: 'utf8' })
+    console.log(contents)
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+logFile()
