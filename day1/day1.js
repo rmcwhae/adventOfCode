9e2e3b54593e49770333709c547c848af81d0d78
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-07 21:29:43 -0800

    Part 2

--- day1/day1.js
+++ day1/day1.js
@@ -11,6 +11,9 @@ async function logFile() {
             .map(values => values.map(value => Number(value)))
             .map(values => values.reduce((a, b) => a + b, 0))
         console.log('The most calories carried by an elf is', Math.max(...splitByLineGaps))
+        const sorted = splitByLineGaps.sort((a, b) => b - a)
+        const top3Sum = sorted.slice(0, 3).reduce((a, b) => a + b, 0)
+        console.log('The top three elves carry this many calories', top3Sum)
     } catch (err) {
         console.error(err.message)
     }
