7b4a2a0a50adc57d30e4282527d103b4c6b2d243
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-13 19:25:58 -0800

    Formatting

--- openfile.js
+++ openfile.js
@@ -5,6 +5,7 @@ async function processFile(file) {
     try {
         const filePath = resolve(file)
         const fileContents = await readFile(filePath, { encoding: 'utf8' })
+        
         return fileContents.split('\n')
     } catch (err) {
         console.error(err.message)
