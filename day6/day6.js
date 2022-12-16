fd8d6cf6cb4d678bdf0ea83c721b4fbd4c123f4d
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-15 19:03:15 -0800

    Part 1 solution

--- day6/day6.js
+++ day6/day6.js
@@ -1,3 +1,20 @@
 const string = require('./input')
 
-console.log(string)
+
+const characterArray = Array.from(string)
+
+console.log({ characterArray })
+
+for (let i = 3; i < characterArray.length; i++) {
+    const previousFourCharacters = characterArray.slice(i - 3, i + 1)
+    if (isAllUnique(previousFourCharacters)) {
+        console.log("The code starts after character", i + 1)
+        return
+    }
+}
+
+function isAllUnique(characters) {
+    const uniqueItems = new Set(characters)
+
+    return Array.from(uniqueItems).length === characters.length
+}
