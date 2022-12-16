2bae0f28a242a7846b026c034d413a794d11785c
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-15 19:10:02 -0800

    Day 6 solution

--- day6/day6.js
+++ day6/day6.js
@@ -1,18 +1,24 @@
 const string = require('./input')
 
-
 const characterArray = Array.from(string)
 
-for (let i = 3; i < characterArray.length; i++) {
-    const previousFourCharacters = characterArray.slice(i - 3, i + 1)
-    if (isAllUnique(previousFourCharacters)) {
-        console.log("The code starts after character", i + 1)
-        return
-    }
-}
+const part1Count = processArray(characterArray, 4)
+console.log("Part 1 count", part1Count)
+const part2Count = processArray(characterArray, 14)
+console.log("Part 2 count", part2Count)
 
 function isAllUnique(characters) {
     const uniqueItems = new Set(characters)
 
     return Array.from(uniqueItems).length === characters.length
+}
+
+function processArray(array, length) {
+    const adjustedIndex = length - 1
+    for (let i = adjustedIndex; i < array.length; i++) {
+        const previousCharacters = characterArray.slice(i - adjustedIndex, i + 1)
+        if (isAllUnique(previousCharacters)) {
+            return i + 1
+        }
+    }
 }
