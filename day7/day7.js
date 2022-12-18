b0651d85616f3bd9e57c2eb3ccab938bad6df4a7
Russell McWhae <russell.mcwhae@gmail.com>
2022-12-17 21:49:48 -0800

    Broken commit

--- day7/day7.js
+++ day7/day7.js
@@ -1,5 +1,38 @@
 const processFile = require("../openfile")
 
-processFile('./input.txt').then(contents => {
-    console.log(contents)
+processFile('./input.txt').then(commands => {
+
+    
+    const groupedCommands = groupIntoCommands(commands)
+    console.log(groupedCommands)
+    // for (const command of groupedCommands) {
+    //     console.log(command)
+    //     processCommand(command)
+    // }
+
 })
+
+function groupIntoCommands(commands) {
+    let groupedCommands = {}
+
+    for (let i = 0; i < commands.length; i++) {
+        const currentCommandNumber = Array.from(groupedCommands).length + 1
+        console.log({currentCommandNumber})
+        if (commands[i][0] === '$') {
+            // Create a new group
+            groupedCommands[currentCommandNumber + 1] = [commands[i]]
+        } else {
+            // Keep adding to the last group
+            if (i > 1) {
+                groupedCommands[currentCommandNumber] = [...groupedCommands[currentCommandNumber], commands[i]]
+            }
+        }
+        console.log({groupedCommands})
+    }
+
+    return groupedCommands
+
+}
+
+function processCommand(command) {
+}
